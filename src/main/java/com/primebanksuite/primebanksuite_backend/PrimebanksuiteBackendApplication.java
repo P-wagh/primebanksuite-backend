a784af843908e18f8381b5237956397fa7df1cd9
Pratik <pwagh1710200@gmail.com>
2024-11-26 08:45:52 +0530

    Initial commit

---
+++ src/main/java/com/primebanksuite/primebanksuite_backend/PrimebanksuiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.primebanksuite.primebanksuite_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimebanksuiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimebanksuiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primebanksuite/primebanksuite_backend/PrimebanksuiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primebanksuite.primebanksuite_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimebanksuiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
